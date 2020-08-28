f332795b20edf0b98474d9695e10797de5136ad0
ankachak <ankan129549@gmail.com>
2020-08-28 12:45:56 +0530

    Dynamo DB Changes

--- src/main/java/com/example/demo/RestWithSwagApplication.java
+++ src/main/java/com/example/demo/RestWithSwagApplication.java
@@ -2,8 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.example.demo.persistence.DynamoDB.repositories.ProductInfoRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+                ProductInfoRepository.class})})
 public class RestWithSwagApplication {
 
 	public static void main(String[] args) {
